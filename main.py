3f4cfb019bfa0c87d232e00bf88749240ae02fdd
danzharuk1 <67105300+danzharuk1@users.noreply.github.com>
2022-07-24 22:07:53 +0300

    Добавление функции проверки ответа, и заглушки к будущей функции проверки числа

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from random import *
 
 rand_num = randint(1, 100)
 
+
+def yes_or_no():
+    while True:
+
+        t = input().lower()
+
+        if t == 'да':
+            return True
+
+        elif t == 'нет':
+            return False
+
+def is_valid(a):
+    pass
+
 print('Игра в угадывание чисел')
 
 while True:
